3e22de34510e06c2649b1d66f8155d324083ab34
onemotre <20221597@stu.neu.edu.cn>
2023-10-02 21:23:26 +0800

    modified:   transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
+declare -A options
+
+while getopts "h:t:i:u:" opt; do
+    case "$opt" in
+        h)
+            echo "./$0 <option> value"
+            echo "--------------------------------------------------------------------"
+            echo "-h                                  Print the help"
+            echo "-t [configure name]                 Transfer the configure into toml"
+            echo "-i [configure name] -u [username]   Install the configure"
+            exit 1
+            ;;
+        t)
+            options["t"]=$OPTARG
+            alacritty migrate -c ./themes_yml/$OPTARG.yml
+            mv ./themes_yml/$OPTARG.toml ./alacritty.toml
+            exit 1
+            ;;
+        i)
+            options["i"]=$OPTARG
+            ;;
+        u)
+            options["u"]=$OPTARG
+            ;;
+        \?)
+            echo "unkown option"
+            exit 1
+            ;;
+        esac
+done
+
 if [ "$EUID" -ne 0 ]; then
   sudo "$0" "$@"
   exit $?
 fi
-
-declare -A options
-
-while getopts "h:t:"; do
-	case "$opt" in
-		h)
-			echo "this is a script to switch the yml theme to toml"
-			echo "this program need root permission"
-			echo "------------------------------------------------"
-			echo "$0 <option> values"
-			;;
-		t)
-			options["t"]=$OPTARG
-			if [-e "./alacritty"]
-			fi
-			;;
-		\?)
-			echo "wrong options"
-	esac
-done
+echo "${options["i"]}"
+echo "${options["u"]}"
+alacritty migrate -c ./themes_yml/${options["i"]}.yml
+mv ./themes_yml/${options["i"]}.toml /home/${options["u"]}/.config/alacritty/alacritty.toml
